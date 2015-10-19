e1132c7ed552702484e220ecb731ad3780eac4ce
bin456789 <bin456789@gmail.com>
2015-10-20 00:20:31 +0800

    Support Wechat 6.3.5

--- app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
+++ app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
@@ -10,7 +10,11 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
 
 public class Main implements IXposedHookLoadPackage {
 
@@ -20,40 +24,67 @@ public class Main implements IXposedHookLoadPackage {
         if (!lpparam.packageName.equals("com.tencent.mm"))
             return;
 
+        //get version
+        Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+        Context systemContext = (Context) callMethod(activityThread, "getSystemContext");
+        String versionName = systemContext.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
+
+        //hook args
+        String className = "com.tencent.mm.sdk.platformtools.";
+        String functionName;
+
+        //only support 6.2.5+
+        if (versionName.compareTo("6.2.5") < 0) {
+            return;
+        }
+
+        //6.2.5
+        if (versionName.startsWith("6.2.5")) {
+            className += "p";
+            functionName = "B";
+        }
+        //6.3.0, 6.3.5+
+        else {
+            className += "q";
+            functionName = "C";
+        }
+
 
         //get context
         findAndHookMethod("com.tencent.mm.ui.LauncherUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                context = (Context) param.thisObject;
-            }
-        });
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        context = (Context) param.thisObject;
+                    }
+                }
+        );
 
 
         //map
-        findAndHookMethod("com.tencent.mm.sdk.platformtools.p", lpparam.classLoader, "B", String.class, String.class, String.class, new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Map map = (Map) param.getResult();
-                if (map == null)
-                    return;
+        findAndHookMethod(className, lpparam.classLoader, functionName, String.class, String.class, String.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Map map = (Map) param.getResult();
+                        if (map == null)
+                            return;
 
-                String type = (String) map.get(".sysmsg.$type");
-                if (type == null)
-                    return;
+                        String type = (String) map.get(".sysmsg.$type");
+                        if (type == null)
+                            return;
 
-                if (type.equals("revokemsg")) {
-                    map.put(".sysmsg.$type", null);
-                    param.setResult(map);
+                        if (type.equals("revokemsg")) {
+                            map.put(".sysmsg.$type", null);
+                            param.setResult(map);
 
-                    //Toast
-                    if (context != null) {
-                        String replacemsg = (String) map.get(".sysmsg.revokemsg.replacemsg");
-                        replacemsg = replacemsg.replaceAll("撤回了一条消息$", "尝试撤回一条消息");
-                        Toast.makeText(context, replacemsg, Toast.LENGTH_LONG).show();
+                            //Toast
+                            if (context != null) {
+                                String replacemsg = (String) map.get(".sysmsg.revokemsg.replacemsg");
+                                replacemsg = replacemsg.replaceAll("撤回了一条消息$", "尝试撤回一条消息");
+                                Toast.makeText(context, replacemsg, Toast.LENGTH_LONG).show();
+                            }
+                        }
                     }
                 }
-            }
-        });
+        );
     }
 }
