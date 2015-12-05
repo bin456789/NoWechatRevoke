a1905663d3b3f8f4a6ede047e9b4d1b5b6d2386a
bin456789 <bin456789@gmail.com>
2015-12-05 14:06:32 +0800

    Add Notification. Add QQ Support.

--- app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
+++ app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
@@ -12,8 +12,7 @@ import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -28,140 +27,112 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class Main implements IXposedHookLoadPackage {
 
-    Context context;
-    Activity activity;
+    Context mmContext;
+    Activity mmActivity;
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.tencent.mm"))
-            return;
+        if (lpparam.packageName.equals("com.tencent.mm")) {
 
-        //get version
-        Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
-        Context systemContext = (Context) callMethod(activityThread, "getSystemContext");
-        String versionName = systemContext.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
+            //get version
+            Object activityThread = callStaticMethod(findClass("android.app.ActivityThread", null), "currentActivityThread");
+            Context systemContext = (Context) callMethod(activityThread, "getSystemContext");
+            String versionName = systemContext.getPackageManager().getPackageInfo(lpparam.packageName, 0).versionName;
 
-        //hook args
-        String className = "com.tencent.mm.sdk.platformtools.";
-        String functionName;
+            //hook args
+            String className = "com.tencent.mm.sdk.platformtools.";
+            String functionName;
 
-        //only support 6.2.5+
-        if (versionName.compareTo("6.2.5") < 0) {
-            return;
-        }
+            //only support 6.2.5+
+            if (versionName.compareTo("6.2.5") < 0) {
+                return;
+            }
 
-        //6.2.5
-        if (versionName.startsWith("6.2.5")) {
-            className += "p";
-            functionName = "B";
-        }
-        //6.3.0, 6.3.5
-        else if (versionName.startsWith("6.3.0")
-                || versionName.startsWith("6.3.5")) {
-            className += "q";
-            functionName = "C";
-        }
-        //6.3.7
-        else {
-            className += "r";
-            functionName = "H";
-        }
+            //6.2.5
+            if (versionName.startsWith("6.2.5")) {
+                className += "p";
+                functionName = "B";
+            }
+            //6.3.0, 6.3.5
+            else if (versionName.startsWith("6.3.0")
+                    || versionName.startsWith("6.3.5")) {
+                className += "q";
+                functionName = "C";
+            }
+            //6.3.7
+            else {
+                className += "r";
+                functionName = "H";
+            }
 
-        //bundle
-        findAndHookMethod("com.tencent.mm.ui.chatting.ChattingUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-                        Log.e("bundle", "aaaaaaaaaaaaaa");
-
-////
-                    }
-
-                }
-        );
-
-        //get context
-        findAndHookMethod("com.tencent.mm.ui.LauncherUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        Log.e("bundle", "onCreate");
-                        context = (Context) param.thisObject;
-                        activity = (Activity) context;
-
-////                        LinearLayout layout = (LinearLayout)thiz.findViewById(0x7F0F005A);
-//                            LinearLayout localLinearLayout = new LinearLayout(context);
-//                            TextView textView = new TextView(thiz);
-//                            textView.setText("aaa");
-//                            localLinearLayout.addView(textView);
-//                            thiz.addContentView(localLinearLayout, null);
-
+            //get context
+            findAndHookMethod("com.tencent.mm.ui.LauncherUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            mmContext = (Context) param.thisObject;
+                            mmActivity = (Activity) mmContext;
+                        }
 
                     }
-
-                }
-        );
+            );
 
 
-        //map
-        findAndHookMethod(className, lpparam.classLoader, functionName, String.class, String.class, String.class, new XC_MethodHook() {
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+            //map
+            findAndHookMethod(className, lpparam.classLoader, functionName, String.class, String.class, String.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            Map map = (Map) param.getResult();
+                            if (map == null)
+                                return;
 
-//
-//                        if (activity == null) {
-//                            Toast.makeText(context, "fuck", Toast.LENGTH_LONG).show();
-//                        }
-//
+                            String type = (String) map.get(".sysmsg.$type");
+                            if (type == null)
+                                return;
 
-                        Map map = (Map) param.getResult();
-                        if (map == null)
-                            return;
+                            if (type.equals("revokemsg")) {
+                                map.put(".sysmsg.$type", null);
+                                param.setResult(map);
 
-                        String type = (String) map.get(".sysmsg.$type");
-                        if (type == null)
-                            return;
+                                //Toast
+                                if (mmContext != null) {
+                                    String replacemsg = (String) map.get(".sysmsg.revokemsg.replacemsg");
+                                    replacemsg = replacemsg.replaceAll("撤回了一条消息$", "尝试撤回一条消息");
+                                    Toast.makeText(mmContext, replacemsg, Toast.LENGTH_LONG).show();
 
-                        if (type.equals("revokemsg")) {
-
-                            Collection<Object> c = map.values();
-                            Iterator it = c.iterator();
-                            while (it.hasNext()) {
-                                Object o = it.next();
-                                if (o != null)
-                                    Log.d("aaa", o.toString());
-                            }
-
-
-                            map.put(".sysmsg.$type", null);
-                            param.setResult(map);
-
-                            //Toast
-                            if (context != null) {
-                                String replacemsg = (String) map.get(".sysmsg.revokemsg.replacemsg");
-                                replacemsg = replacemsg.replaceAll("撤回了一条消息$", "尝试撤回一条消息");
-                                Toast.makeText(context, replacemsg, Toast.LENGTH_LONG).show();
-
-
-                                NotificationCompat.Builder mBuilder =
-                                        new NotificationCompat.Builder(context)
-                                                .setAutoCancel(true)
-                                                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                                                .setContentTitle("微信撤回")
-                                                .setContentText(replacemsg).setDefaults(Notification.DEFAULT_ALL);
-                                Intent resultIntent = new Intent(activity, activity.getClass());
-
-
-                                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-                                stackBuilder.addParentStack(activity.getClass());
-                                stackBuilder.addNextIntent(resultIntent);
-                                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-                                mBuilder.setContentIntent(resultPendingIntent);
-                                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                                mNotificationManager.notify(11111, mBuilder.build());
+                                    //Notification
+                                    NotificationCompat.Builder mBuilder =
+                                            new NotificationCompat.Builder(mmContext)
+                                                    .setAutoCancel(true)
+                                                    .setSmallIcon(android.R.drawable.ic_dialog_info)
+                                                    .setContentTitle("微信撤回")
+                                                    .setContentText(replacemsg).setDefaults(Notification.DEFAULT_ALL);
+                                    Intent resultIntent = new Intent(mmActivity, mmActivity.getClass());
+                                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(mmContext);
+                                    stackBuilder.addParentStack(mmActivity.getClass());
+                                    stackBuilder.addNextIntent(resultIntent);
+                                    PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                                    mBuilder.setContentIntent(resultPendingIntent);
+                                    NotificationManager mNotificationManager = (NotificationManager) mmContext.getSystemService(Context.NOTIFICATION_SERVICE);
+                                    mNotificationManager.notify(0, mBuilder.build());
+                                }
                             }
                         }
                     }
-                }
-        );
+            );
+        }
+
+        if (lpparam.packageName.equals("com.tencent.mobileqq")) {
+
+            //hook args
+            String className = "com.tencent.mobileqq.app.message.QQMessageFacade";
+            String functionName = "a";
+
+            findAndHookMethod(className, lpparam.classLoader, functionName, ArrayList.class, boolean.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(null);
+                        }
+                    }
+            );
+        }
     }
 }
