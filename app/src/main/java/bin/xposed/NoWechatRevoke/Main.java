f9b5ba6a42354db4b1a0d06fada8441532641d6e
bin456789 <bin456789@gmail.com>
2015-12-05 13:52:29 +0800

    Add Notification

--- app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
+++ app/src/main/java/bin/xposed/NoWechatRevoke/Main.java
@@ -1,9 +1,19 @@
 package bin.xposed.NoWechatRevoke;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -19,6 +29,7 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 public class Main implements IXposedHookLoadPackage {
 
     Context context;
+    Activity activity;
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.tencent.mm"))
@@ -55,13 +66,38 @@ public class Main implements IXposedHookLoadPackage {
             functionName = "H";
         }
 
+        //bundle
+        findAndHookMethod("com.tencent.mm.ui.chatting.ChattingUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
+
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                        Log.e("bundle", "aaaaaaaaaaaaaa");
+
+////
+                    }
+
+                }
+        );
 
         //get context
         findAndHookMethod("com.tencent.mm.ui.LauncherUI", lpparam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        Log.e("bundle", "onCreate");
                         context = (Context) param.thisObject;
+                        activity = (Activity) context;
+
+////                        LinearLayout layout = (LinearLayout)thiz.findViewById(0x7F0F005A);
+//                            LinearLayout localLinearLayout = new LinearLayout(context);
+//                            TextView textView = new TextView(thiz);
+//                            textView.setText("aaa");
+//                            localLinearLayout.addView(textView);
+//                            thiz.addContentView(localLinearLayout, null);
+
+
                     }
+
                 }
         );
 
@@ -70,6 +106,13 @@ public class Main implements IXposedHookLoadPackage {
         findAndHookMethod(className, lpparam.classLoader, functionName, String.class, String.class, String.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+
+//
+//                        if (activity == null) {
+//                            Toast.makeText(context, "fuck", Toast.LENGTH_LONG).show();
+//                        }
+//
+
                         Map map = (Map) param.getResult();
                         if (map == null)
                             return;
@@ -79,6 +122,16 @@ public class Main implements IXposedHookLoadPackage {
                             return;
 
                         if (type.equals("revokemsg")) {
+
+                            Collection<Object> c = map.values();
+                            Iterator it = c.iterator();
+                            while (it.hasNext()) {
+                                Object o = it.next();
+                                if (o != null)
+                                    Log.d("aaa", o.toString());
+                            }
+
+
                             map.put(".sysmsg.$type", null);
                             param.setResult(map);
 
@@ -87,6 +140,24 @@ public class Main implements IXposedHookLoadPackage {
                                 String replacemsg = (String) map.get(".sysmsg.revokemsg.replacemsg");
                                 replacemsg = replacemsg.replaceAll("撤回了一条消息$", "尝试撤回一条消息");
                                 Toast.makeText(context, replacemsg, Toast.LENGTH_LONG).show();
+
+
+                                NotificationCompat.Builder mBuilder =
+                                        new NotificationCompat.Builder(context)
+                                                .setAutoCancel(true)
+                                                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                                                .setContentTitle("微信撤回")
+                                                .setContentText(replacemsg).setDefaults(Notification.DEFAULT_ALL);
+                                Intent resultIntent = new Intent(activity, activity.getClass());
+
+
+                                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                                stackBuilder.addParentStack(activity.getClass());
+                                stackBuilder.addNextIntent(resultIntent);
+                                PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                                mBuilder.setContentIntent(resultPendingIntent);
+                                NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                                mNotificationManager.notify(11111, mBuilder.build());
                             }
                         }
                     }
